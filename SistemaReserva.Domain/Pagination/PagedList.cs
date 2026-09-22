8ae591d44ac7e1c525ae22d29aabf6a33e419499
RafaelSantos28d <rh28.09.2007@gmail.com>
2026-09-22 10:04:07 -0300

    feat: implementa validação de pageSize e pageNumber de PagedList

--- SistemaReserva.Domain/Pagination/PagedList.cs
+++ SistemaReserva.Domain/Pagination/PagedList.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SistemaReserva.Domain.Exceptions;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace SistemaReserva.Domain.Pagination
@@ -8,17 +10,22 @@ namespace SistemaReserva.Domain.Pagination
     {
         public PagedList(IEnumerable<T> items, int currentPage, int pageSize, int totalCount)
         {
-            CurrentPage = currentPage;
-            PageSize = pageSize;
+            Validation(currentPage, pageSize);
             TotalCount = totalCount;
             TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
             Items = items;
         }
-
+        
         public int CurrentPage { get; set; }
         public int PageSize { get; set; }
         public int TotalCount { get; set; }
         public int TotalPages { get; set; }
         public IEnumerable<T> Items { get; set; } = [];
+
+        public void Validation(int currentPage, int pageSize)
+        {
+            DomainValidationException.When(currentPage < 1, "A página deve ser maior que 0");
+            DomainValidationException.When(pageSize > 50, "O tamanho da página deve ser maior que 0 no máximo 50");
+        }
     }
 }
